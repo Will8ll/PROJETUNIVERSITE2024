807bfafcfb61c4189aafe24a520c0991d838e574
Will8ll <wilfriedkoffiac@gmail.com>
2024-04-12 01:04:00 +0300

    pipDjangoUpdate

--- UEPME/info.py
+++ UEPME/info.py
@@ -1,6 +1,8 @@
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_TLS = True
+#EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+#EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_HOST_USER = 'info.uspme@gmail.com' # Your Gmail username
 EMAIL_HOST_PASSWORD = 'wesn kdpl utlc gsqk'# Your Gmail password or app password
