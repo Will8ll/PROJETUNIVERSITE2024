e4261b694b10d7264caa1096fd69c7f14e19d859
Will8ll <wilfriedkoffiac@gmail.com>
2024-04-11 04:17:40 +0300

    ed25519 1.4

--- UEPME/settings.py
+++ UEPME/settings.py
@@ -90,10 +90,12 @@ WSGI_APPLICATION = 'UEPME.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 
 # Password validation
