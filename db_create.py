f519e0e08ba49c93dc33645368669eacd7e37d95
Will8ll <wilfriedkoffiac@gmail.com>
2024-04-11 08:33:31 +0300

    db updated

--- db_create.py
+++ db_create.py
@@ -1,2 +1,9 @@
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UEPME.settings')
+django.setup()
+
+from django.contrib.auth.models import User
 from UEPME import db
 db.create_all()
