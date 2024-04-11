8e4ee1dc94b281e807c5275515d308912e7afd91
Will8ll <wilfriedkoffiac@gmail.com>
2024-04-11 08:20:18 +0300

    settings update

--- UEPME/settings.py
+++ UEPME/settings.py
@@ -90,6 +90,9 @@ WSGI_APPLICATION = 'UEPME.wsgi.application'
 
 
 
+#DATABASES = {
+ #   'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#}
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
