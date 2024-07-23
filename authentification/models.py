a6e8a61f7bd001fce813e5262241db988e1096f9
Will8ll <wilfriedkoffiac@gmail.com>
2024-07-23 00:31:36 +0000

    v

--- authentification/models.py
+++ authentification/models.py
@@ -12,10 +12,10 @@ class Subscriber(models.Model):
         return self.email
     
 class Registration(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    contact_number = models.CharField(max_length=20)
+    contact = models.CharField(max_length=20)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
