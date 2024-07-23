6e655275dfb8941475ea54fa4f36c91fcc18f3e2
Will8ll <wilfriedkoffiac@gmail.com>
2024-07-23 02:33:58 +0000

    last test 02h23

--- authentification/models.py
+++ authentification/models.py
@@ -15,7 +15,7 @@ class Registration(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField()
-    contact_number = models.CharField(max_length=20)
+    contact_number = models.CharField(max_length=15)  
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} - {self.email}"
